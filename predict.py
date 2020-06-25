1fa3b345ccf2c64c7a28315f67c2398ea8ea0ad3
Andress-Carranza <63977242+Andress-Carranza@users.noreply.github.com>
2020-06-24 21:13:00 -0700

    revised model

--- predict.py
+++ predict.py
@@ -1,55 +1,74 @@
 import tensorflow as tf
 from tensorflow import keras
-import train_model as tm
-import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import train_model as tm
 
-model = keras.models.load_model('models/simple_model')
+def load_rpm_data(start_index, end_index)  :
+    data = tm.get_normalized_data()
+    print('\n\n\nLoaded data')
 
-max_vals, data = tm.get_normalized_data()
+    if start_index < 0:
+        start_index = len(data.index) + start_index
+    if end_index < 0:
+        end_index = len(data.index) + end_index 
+    elif end_index == 0:
+        end_index = len(data.index)
+        
+    return data.loc[start_index:end_index]
 
-print('Loaded data\n\n\n')
-
-
-label_name = 'RPMs'
-
-features = {name:np.array(value) for name, value in data.items()}
-label = np.array(features.pop(label_name))
-
-
-prediction = model.predict(features)
-
-results = pd.DataFrame()
-results['Date'] = pd.read_csv('data\\dummy_vars.csv')['Date'][:]
-results['Label'] = (label * max_vals['RPMs'])[:]
-results['Prediction'] = (prediction * max_vals['RPMs'])[:]
- 
-results = results.reset_index()   
-
-fig = plt.figure(figsize=(15,2))
-axes = fig.add_subplot(111)
-
-axes.set_title('Monthly Domestic RPMs Time Series Model')
     
-plt.xlabel('Date')
-plt.ylabel('RPMs (billions)')
-    
-low = min(results['Label'])/1000000000
-high = max(results['Label'])/1000000000
-rng = high-low
-axes.set_ylim(int(low-rng*.1), int(high+rng*.1))
+def predict(features,model):
+    max_vals = tm.get_max_vals()
 
-frq = 12
-step = max(int(len(results['Date'])/frq),1)
-tck = range(0,len(results['Date']), step)
-tck_dates = []
-for i in tck:
-    tck_dates.append(results['Date'][i])
-plt.xticks(tck, tck_dates)
+    features = {name:np.array(value) for name, value in features.items()}
+    label = np.array(features.pop('RPMs'))
+    date = np.array(features.pop('Date'))
     
-axes.plot(results['Date'], results['Label']/1000000000, label='Actual RPMs')
-axes.plot(results['Date'], results['Prediction']/1000000000, label='Predicted RPMs')
-plt.legend(loc="upper left")
+    prediction = model.predict(features)
+    print('Made prediction')
 
-plt.show()
+    results = pd.DataFrame()
+    
+    results['Date'] = date
+    
+    results['Label'] = label * max_vals['RPMs']
+    
+    results['Prediction'] = prediction * max_vals['RPMs']
+    
+    results = results.reset_index()
+    return results
+
+def plot_results(results):
+    fig = plt.figure(figsize=(15,2))
+    axes = fig.add_subplot(111)
+    
+    axes.set_title('Monthly Domestic RPMs Time Series Model')
+        
+    plt.xlabel('Date')
+    plt.ylabel('RPMs (billions)')
+        
+    low = min(results['Label'])/1000000000
+    high = max(results['Label'])/1000000000
+    rng = high-low
+    axes.set_ylim(int(low-rng*.1), int(high+rng*.1))
+    
+    frq = 12
+    step = max(int(len(results['Date'])/frq),1)
+    tck = range(0,len(results['Date']), step)
+    tck_dates = []
+    for i in tck:
+        tck_dates.append(results['Date'][i])
+    plt.xticks(tck, tck_dates)
+        
+    axes.plot(results['Date'], results['Label']/1000000000, label='Actual RPMs')
+    axes.plot(results['Date'], results['Prediction']/1000000000, label='Predicted RPMs')
+    plt.legend(loc="upper left")
+    
+    plt.show()
+    
+model = keras.models.load_model('models/model')
+
+plot_results(predict(load_rpm_data(0,0), model))
+
--- train_model.py
+++ train_model.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from matplotlib import pyplot as plt
 import seaborn as sns
+import math
 
 def load_data():
     model_data = pd.read_csv('data\\raw\\model_data.csv')
@@ -27,16 +28,19 @@ def load_data():
     
     model_data.to_csv('model_data.csv', index = False)
 
+def apply_inverse(training_data):
+    #Inverse squares of normalized data
+    training_data['Months_After_Covid-19'] = 1 / training_data['Months_After_Covid-19'] ** -.5
+    training_data['Months_After_9/11'] = 1 / training_data['Months_After_9/11'] **2
+    training_data = training_data.replace(math.inf,0)
+    return training_data
+
 def get_normalized_data():
     training_data = pd.read_csv('data\\training_data.csv')
-    max_vals = {'Months_After_Covid-19': max(training_data['Months_After_Covid-19']),
-                'Months_After_9/11': max(training_data['Months_After_9/11']),
-                'CPG': max(training_data['CPG']),
-                'LCC_Market_Share': max(training_data['LCC_Market_Share']),
-                'Unemployement': max(training_data['Unemployement']),
-                'Labor_Force': max(training_data['Labor_Force']),
-                'RPMs': max(training_data['RPMs'])}
     
+
+    training_data = apply_inverse(training_data)
+
     training_data['Months_After_Covid-19'] = training_data['Months_After_Covid-19'] / max(training_data['Months_After_Covid-19'])
     training_data['Months_After_9/11'] = training_data['Months_After_9/11'] / max(training_data['Months_After_9/11'])
 
@@ -49,13 +53,24 @@ def get_normalized_data():
     training_data['Labor_Force'] = training_data['Labor_Force'] / max(training_data['Labor_Force'])
 
     training_data['RPMs'] = training_data['RPMs'] / max(training_data['RPMs'])
- 
     
-    return max_vals, training_data
+    return training_data
 
+def get_max_vals():
+    training_data = pd.read_csv('data\\training_data.csv')
+    
+    training_data = apply_inverse(training_data)
+
+    return { 'Months_After_Covid-19': max(training_data['Months_After_Covid-19']),
+                'Months_After_9/11': max(training_data['Months_After_9/11']),
+                'CPG': max(training_data['CPG']),
+                'LCC_Market_Share': max(training_data['LCC_Market_Share']),
+                'Unemployement': max(training_data['Unemployement']),
+                'Labor_Force': max(training_data['Labor_Force']),
+                'RPMs': max(training_data['RPMs'])}    
 def get_feature_layer(data):
     feature_columns = []
-    for column in data.columns[:-1]:
+    for column in data.columns[1:-2]:
         feature_columns.append(tf.feature_column.numeric_column(column))
     return tf.keras.layers.DenseFeatures(feature_columns)
 
@@ -74,16 +89,19 @@ def create_model(learning_rate, feature_layer):
     
     model.add(feature_layer)
     
-    # Define the first hidden layer with 20 nodes.   
-    model.add(tf.keras.layers.Dense(units=20, 
+
+    
+    model.add(tf.keras.layers.Dense(units=200, 
                                   activation='relu', 
-                                  name='Hidden1'))
-  
-    # Define the second hidden layer with 12 nodes. 
-    model.add(tf.keras.layers.Dense(units=12, 
+                                  name='Hidden3'))
+    model.add(tf.keras.layers.Dropout(rate=0.07))
+    model.add(tf.keras.layers.Dense(units=128, 
                                   activation='relu', 
-                                  name='Hidden2'))
-  
+                                  name='Hidden4')) 
+
+    model.add(tf.keras.layers.Dense(units=64 ,
+                                  activation='relu', 
+                                  name='Hidden4')) 
     # Define the output layer.
     model.add(tf.keras.layers.Dense(units=1,  
                                   name='Output'))                              
@@ -93,10 +111,11 @@ def create_model(learning_rate, feature_layer):
                 metrics=[tf.keras.metrics.MeanSquaredError()])
 
     return model
-
 def train_model(model, dataset, epochs, label_name, batch_size=None):
     # Split the dataset into features and label.
+    del dataset['Date']
     features = {name:np.array(value) for name, value in dataset.items()}
+    
     label = np.array(features.pop(label_name))
 
     history = model.fit(x=features, y=label, batch_size=batch_size,epochs=epochs) 
@@ -112,7 +131,7 @@ def train_model(model, dataset, epochs, label_name, batch_size=None):
     return epochs, mse
 
 def train():
-    max_vals, training_data = get_normalized_data()
+    training_data = get_normalized_data()
     print('Normalized Data')
     
     feature_layer = get_feature_layer(training_data)
@@ -120,7 +139,7 @@ def train():
     
     #The following variables are the hyperparameters.
     learning_rate = 0.01
-    epochs = 20
+    epochs = 100
     batch_size = None
     
     model = create_model(learning_rate, feature_layer)
@@ -138,4 +157,5 @@ def train():
     plot_the_loss_curve(epochs, mse)
     
     
-    model.save('models\\simple_model\\')
+    model.save('models\\reg_model\\')
+#train()
